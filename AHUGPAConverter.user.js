913209aa6e1b852a13d34b5a1693d32f6f8c037d
skymkmk <admin@skymkmk.com>
2022-07-03 14:17:49 +0800

    V1.0 update

--- AHUGPAConverter.user.js
+++ AHUGPAConverter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         安大教务四分制绩点转换
 // @namespace    https://www.skymkmk.com
-// @version      0.6
+// @version      1.0
 // @description  将你的平均绩点从五分制转换为四分制，方便你比对心仪海外高校
 // @author       skymkmk
 // @match        *://jwxt0.ahu.edu.cn/*
@@ -37,13 +37,18 @@
     if (point === "合格") return 2;
     if (parseInt(point) > 84) return 4;
     if (parseInt(point) > 74) return 3;
-    if (parseInt(point) > 60) return 2;
+    if (parseInt(point) >= 60) return 2;
     return 0;
   }
   function nodeGen(msg, color = 'black') {
     const node = document.createElement('p');
     node.innerHTML = msg;
     node.style = 'margin: 0; color:' + color + ';';
+    return node;
+  }
+  function formGen(msg) {
+    const node = document.createElement('td');
+    node.innerHTML = msg;
     return node;
   }
   if (table !== null) {
@@ -56,34 +61,63 @@
         creditPKU += parseFloat(i.children[4].innerHTML);
         if (point === "优秀") {
           sumPKU += credit * 3.95;
+          i.insertBefore(formGen('4.5'), i.children[7]);
+          i.insertBefore(formGen('3.95'), i.children[8]);
         } else if (point === "良好") {
           sumPKU += credit * 3.58;
+          i.insertBefore(formGen('3.5'), i.children[7]);
+          i.insertBefore(formGen('3.58'), i.children[8]);
         } else if (point === "中等") {
           sumPKU += credit * 2.83;
+          i.insertBefore(formGen('2.5'), i.children[7]);
+          i.insertBefore(formGen('2.83'), i.children[8]);
         } else if (point === "合格") {
           sumPKU += credit * 1.7;
+          i.insertBefore(formGen('1.5'), i.children[7]);
+          i.insertBefore(formGen('1.7'), i.children[8]);
         } else {
           const pointNum = parseInt(point);
           if (pointNum >= 60) {
             sumPKU += credit * (4 - (3 * (100 - pointNum) ** 2) / 1600);
+            i.insertBefore(formGen(((pointNum - 50) / 10).toString()), i.children[7]);
+            i.insertBefore(formGen((Math.round((4 - (3 * (100 - pointNum) ** 2) / 1600) * 100) / 100).toString()), i.children[8]);
+          }
+          else {
+            i.insertBefore(formGen('0'), i.children[7]);
+            i.insertBefore(formGen('0'), i.children[8]);
           }
         }
         if (!excludeCode.test(code)) {
           if (excludeCourse.test(courseName)) {
             WESExcludedCourse.push(courseName);
+            i.children[4].innerHTML = i.children[4].innerHTML + '<div style="color: red;">WES 排除计算</div>';
+            i.insertBefore(formGen('排除'), i.children[9]);
           } else if (oneCreditCourse.test(courseName)) {
             WESDecreasedCredit.set(courseName, 1);
             creditWES += 1;
             sumWES += WESPointCalculator(point);
+            i.children[4].innerHTML = i.children[4].innerHTML + '<div style="color: red;">WES 降重为 1</div>';
+            i.insertBefore(formGen(WESPointCalculator(point).toString()), i.children[9]);
           } else if (twoCreditCourse.test(courseName)) {
             WESDecreasedCredit.set(courseName, 2);
             creditWES += 2;
             sumWES += 2 * WESPointCalculator(point);
+            i.children[4].innerHTML = i.children[4].innerHTML + '<div style="color: red;">WES 降重为 2</div>';
+            i.insertBefore(formGen(WESPointCalculator(point).toString()), i.children[9]);
           } else {
             creditWES += credit;
             sumWES += credit * WESPointCalculator(point);
+            i.insertBefore(formGen(WESPointCalculator(point).toString()), i.children[9]);
           }
-        } else WESExcludedCourse.push(courseName);
+        } else {
+          WESExcludedCourse.push(courseName);
+          i.children[4].innerHTML = i.children[4].innerHTML + '<div style="color: red;">WES 排除计算</div>';
+          i.insertBefore(formGen('排除'), i.children[9]);
+        }
+      } else {
+        i.insertBefore(formGen('安大绩点'), i.children[7]);
+        i.insertBefore(formGen('北大绩点'), i.children[8]);
+        i.insertBefore(formGen('WES 绩点'), i.children[9]);
       }
     }
     avgPKU = Math.round((sumPKU / creditPKU) * 100) / 100;
